8e16e0630ea0a492940470b914b62f93b3e33b6a
Arikapa <stevenaricapa@outlook.es>
2025-09-25 02:07:29 -0500

    Aún no carga el finPartida.js

--- js/finPartida.js
+++ js/finPartida.js
@@ -1,6 +1,17 @@
+import { obtenerJugadores } from "./ui.js";
+console.log("finPartida.js cargado"); //  Ni siquiera lo muestra :c
+
 document.addEventListener("DOMContentLoaded", () => {
+    
     document.getElementById('btn-restart').addEventListener('click', () => {
         alert('La partida se reiniciará. Serás redirigido a la página principal.');
-        window.location.href = '../index.html';
+        window.location.href = 'index.html';
     });
+
+    document.getElementById('btn-cargar-ranking').addEventListener('click', () => {
+        const jugadores = obtenerJugadores();
+        console.log(jugadores);
+    });
+
 });
+
