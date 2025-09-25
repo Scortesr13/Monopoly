ed9367596fac6b0e4af98d2c0fe9977b3226ddb5
Arikapa <stevenaricapa@outlook.es>
2025-09-25 01:04:21 -0500

    Se soluciona el botón 'reiniciar juego'

--- js/finPartida.js
+++ js/finPartida.js
@@ -1,9 +1,6 @@
-import { obtenerJugadores, renderJugadores, dibujarTablero } from "./ui.js";
-import { obtenerTablero } from "./api.js";
-import { colocarFichas } from "./game.js";
-import { jugadorActual } from "./turnos.js";
-import { jugarTurno, pasarTurno } from "./turnos.js";
-
-document.getElementById('btn-restart').addEventListener('click', function() {
-    window.location.href = './index.html';
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('btn-restart').addEventListener('click', () => {
+        alert('La partida se reiniciará. Serás redirigido a la página principal.');
+        window.location.href = '../index.html';
+    });
 });
